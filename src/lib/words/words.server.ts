682816ee5e9e58ce44f16c49d7cddae36c915aa0
p10 <pio.was@gmail.com>
2024-05-06 10:52:31 +0200

    zod

--- src/lib/words/words.server.ts
+++ src/lib/words/words.server.ts
@@ -1,24 +1,64 @@
 import { fs } from 'zx';
+import { ZodError, z } from 'zod';
 
 type Data = {
   words: Word[];
 };
 
-type Word = {
-  polish: string;
-  espanol: string;
-  variety: 'p1' | 'p2' | 'p3' | 'm1' | 'm2' | 'm3';
-  value: string;
-  createdAt: Date;
-  lastAnswer: { date: Date; isGood: boolean };
-  sumOfGood: number;
-  sumOfBad: number;
-};
+const WordSchema = z.object({
+  polish: z.string().min(2, { message: 'Musi zawierać conajmniej 2 litery' }),
+  espanol: z.string().min(2, { message: 'Musi zawierać conajmniej 2 litery' }),
+  variety: z.enum(['p1', 'p2', 'p3', 'm1', 'm2', 'm3']),
+  value: z.string().min(2, { message: 'Musi zawierać conajmniej 2 litery' }),
+  lastAnswered: z
+    .object({
+      date: z.string().datetime(),
+      isBood: z.boolean(),
+    })
+    .optional(),
+  sumOfGood: z.number(),
+  sumOfBad: z.number(),
+});
+
+type Word = z.infer<typeof WordSchema>;
+
+type AlreadyExistsError = Readonly<{
+  type: 'AlreadyExists';
+  message: string;
+}>;
+
+type ValidationError = Readonly<{
+  type: 'ValidationError';
+  issues: ZodError['issues'];
+}>;
+
+type InputWord = Pick<Word, 'polish' | 'espanol' | 'variety' | 'value'>;
 
 export async function createWords(fileName: string) {
   const data = await readFile(fileName);
 
   return {
+    async add(
+      input: InputWord,
+    ): Promise<undefined | ValidationError | AlreadyExistsError> {
+      if (wordExists(data.words, input)) {
+        return { type: 'AlreadyExists', message: 'word already exists' };
+      }
+      const result = WordSchema.safeParse({
+        ...input,
+        sumOfBad: 0,
+        sumOfGood: 0,
+      });
+      if (result.success) {
+        result.data;
+        await saveFile(fileName, {
+          words: [...data.words, result.data],
+        });
+        return undefined;
+      }
+      return { type: 'ValidationError', issues: result.error.issues };
+    },
+
     list() {
       return data.words;
     },
@@ -29,6 +69,14 @@ function init(): Data {
   return {
     words: [],
   };
+}
+
+function wordExists(list: Word[], word: InputWord) {
+  return (
+    list.find(
+      (w) => word.espanol === w.espanol && word.variety === w.variety,
+    ) !== null
+  );
 }
 
 async function readFile(fileName: string): Promise<Data> {
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,11 @@
 import { createWords } from '$lib/words/words.server';
 
 const words = await createWords('db.json');
-console.log(words);
+
+// words.add({
+//   polish: 'kurwa',
+//   espanol: 'curve',
+//   variety: 'p1',
+//   value: 'curvie',
+// });
+console.log(JSON.stringify(words.list(), null, 2));
